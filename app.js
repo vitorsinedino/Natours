60479d1ad95362bf0b61d1b88871b56a9d731f92
vitorsinedino <vitorsinedino@gmail.com>
2020-04-23 08:17:09 -0700

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'hello from the sever side !!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'hello from the sever side !!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('you can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('you can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
 app.listen(port, () => {
