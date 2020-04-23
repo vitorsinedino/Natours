e0317a2c0512053fb691dd8770fb9c414c6bfc73
vitorsinedino <vitorsinedino@gmail.com>
2020-04-23 08:41:06 -0700

    third commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 const port = 3000;
 
 // app.get('/', (req, res) => {
@@ -26,6 +28,25 @@ app.get("/api/v1/tours", (req, res) => {
       tours
     }
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
